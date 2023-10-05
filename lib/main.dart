ebdadcfb48d4c995ae46e48ef475648c037b077c
sona-user-369 <donadavak@gmail.com>
2023-10-05 21:18:03 +0100

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
 
 void main() {
+
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FlutterSplashScreen(
+        defaultNextScreen: null,
+        splashScreenBody: Image.asset(''),
+        onInit: (){
+
+        },
+
+        onEnd: (){
+
+        },
+
+        setStateCallback: (){
+
+        },
+
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
 
 
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
