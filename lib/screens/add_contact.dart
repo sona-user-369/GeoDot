08e027579756cb28afe9bc1c21f7029c9e397939
sona-user-369 <donadavak@gmail.com>
2023-10-09 21:33:56 +0100

    update pages

--- lib/screens/add_contact.dart
+++ lib/screens/add_contact.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:geodot/controllers/add_contact_controller.dart';
 import 'package:get/get.dart';
 
+import '../utils/colors.dart';
+import '../utils/constants.dart';
+import '../widgets/box_drawer.dart';
+
 
 
 class AddContactPage extends StatefulWidget {
@@ -14,6 +18,7 @@ class AddContactPage extends StatefulWidget {
 class _AddContactPageState extends State<AddContactPage> {
 
   late AddContactController controller ;
+  var _formKey = GlobalKey<FormState>() ;
 
   @override
   void initState() {
@@ -27,7 +32,148 @@ class _AddContactPageState extends State<AddContactPage> {
     return GetBuilder(
       init: controller,
         builder: (controller){
-          return const Scaffold();
+          return  Scaffold(
+            appBar: AppBar(
+              backgroundColor: ColorPicker.success.withOpacity(0.5),
+              title: Text('Add a contact'),
+            ),
+              drawer: BoxDrawer(),
+            body:  Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                  key: _formKey,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            child: const Text(
+                              'Id con of contact :',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              // filled: true,
+                              fillColor: ColorPicker.dark.withOpacity(0.2),
+                              border: InputBorder.none,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: ColorPicker.dark.withOpacity(0.2),
+                                      width: 3
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: ColorPicker.success,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: ColorPicker.danger,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: ColorPicker.danger,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: Constants.defaultPadding,),
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            child: const Text(
+                              'Kindly name (Optional) :',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              // filled: true,
+                              fillColor: ColorPicker.dark.withOpacity(0.2),
+                              border: InputBorder.none,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: ColorPicker.dark.withOpacity(0.2),
+                                      width: 3
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: ColorPicker.success,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: ColorPicker.danger,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: ColorPicker.danger,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: Constants.defaultPadding,),
+
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: ColorPicker.success,
+                                fixedSize: Size(180, 40)
+                            ),
+                            onPressed: (){
+                              Navigator.pushNamed(context, 'contacts');
+                            },
+                            child: const Text(
+                              'Send request',
+                              style: TextStyle(
+                                  color: ColorPicker.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17
+                              ),
+                            )
+                        ),
+
+
+                      ],
+                    ),
+                  )
+              ),
+            )
+          );
         }
     );
   }
