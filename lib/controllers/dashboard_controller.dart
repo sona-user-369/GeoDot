33f94c6d55552249420359c24ee12fcbbe6b922c
sona-user-369 <donadavak@gmail.com>
2023-10-27 23:51:43 +0100

    update pages

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -3,9 +3,16 @@ import 'package:get/get.dart' ;
 class DashBoardController extends GetxController {
   DashBoardController();
 
+  int totalContacts = 0 ;
+  int totalConnected = 0 ;
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
+
   }
+
+
+
 }
